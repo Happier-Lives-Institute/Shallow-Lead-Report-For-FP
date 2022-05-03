c5a5e794aad74bacd7e82928f69a47155dc41777
Karakaii <samu.dupret@hotmail.com>
2022-05-03 09:54:32 +0100

    update graphs and analysis

--- master.R
+++ master.R
@@ -26,6 +26,27 @@ mod.1 <- rma.mv(yi = d, V = d_se^2,
        data = d.childlag)
 
 effect <- coef(mod.1)[[1]]; effect
-linear.growth <- abs(effect/(27-3)); linear.growth
+linear.growth <- abs(effect/(27-2)); linear.growth
 effectAt30 <- effect + 3 * linear.growth; effectAt30
-effectBy30 <- pracma::integral(function(t){0+linear.growth*t}, 3, 30); effectBy30
+effectBy30 <- pracma::integral(function(t){0+linear.growth*t}, 2, 30); effectBy30
+
+figureData <- as.data.frame(c(
+    NA, NA,
+    sapply(0:28, FUN=function(t){0+linear.growth*t}),
+    rep(0.0180, 43)))
+colnames(figureData) <- c("SDs of MHa")
+figureData$`SDs of MHa` <- -figureData$`SDs of MHa`
+figureData$years <- 0:73
+
+p <- ggplot(figureData, aes(y=`SDs of MHa`, x=years)) +
+    geom_line() +
+    theme_cowplot() +
+    scale_y_continuous(limits = c(-0.02, 0)) +
+    scale_x_continuous(breaks=seq(0, 75, 5), limits = c(0, 75))
+p
+
+ggsave(filename = paste0("causalgraphs/Figure 3_prep.png"),
+       plot = p,
+       width = 8,
+       height = 8,
+       dpi = 1200)
